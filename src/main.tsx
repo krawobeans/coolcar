1ae9a7303a083dcdcb1c66992ccfd770141aa67b
Cool Car Auto <coolcarauto.info@gmail.com>
2025-01-29 12:43:09 +0000

    feat: add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App'
 import './index.css'
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(rootElement).render(
     <Router>
       <App />
       <Analytics />
+      <SpeedInsights />
     </Router>
   </React.StrictMode>,
 )
