d78291e5587dfa6bba1a34a6aded2a8a52cab77f
Cool Car Auto <coolcarauto.info@gmail.com>
2025-01-29 12:32:51 +0000

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <Router>
       <App />
+      <Analytics />
     </Router>
   </React.StrictMode>,
 )
